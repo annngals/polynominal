e9dc98ad4de0fee0e30ab2cbdcdb39dc99eb0e45
annngals <57696371+annngals@users.noreply.github.com>
2020-07-10 13:47:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 #include <locale.h>
 
 using namespace std;
-
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	int coff1[] = { 4,5,2,7 };		//ввод значений для массива многочлена №1
+	int coff1[] = { 2,3,4,3 };		//ввод значений для массива многочлена №1
 	int coff2[] = { 3,6,2 };		//ввод значений для массива многочлена №2
 
 	Poly poly1(coff1, 4);		//определение многочлена №1
@@ -19,7 +18,7 @@ int main()
 	
 	cout << "Solve poly1: " << poly1.Solve(2) << endl;	//вывод решения многочлена №1 со значением аргумента = 2
 	cout << "Solve poly2: " << poly2.Solve(2) << endl;	//вывод решения многочлена №2 со значением аргумента = 2
-	cout << "Check poly2: " << poly1.Check(70, 80) << endl;		//проверка решения на интервале
+	cout << "Check poly2: " << poly1.Check(-1.0, 2.0) << endl;	//проверка решения на интервале
 	cout << "Sum: " << poly1 + poly2 << endl;	//вывод суммы многочленов
 	cout << "Diff: " << poly1 - poly2 << endl;	//вывод разности многочленов
 	cout << "Integrate poly2: " << poly1.Integrate(0, 1) << endl;		//вывод интеграла многочлена №2
